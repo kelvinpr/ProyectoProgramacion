842d480ceae3060df2d0a022d89339358cb2b5d9
Kelvin <90300543+kelvinpr@users.noreply.github.com>
2026-01-21 19:40:05 -0500

    ...

---
+++ src/main/java/DAO/ExceptionDAO.java
@@ -0,0 +1,10 @@
+package DAO;
+
+public class ExceptionDAO extends Exception{
+    public ExceptionDAO(String msg) {
+        super(msg);
+    }
+    public ExceptionDAO() {
+        super();
+    }
+}
---
+++ src/main/java/ImplementacionDAO/ExceptionDAO.java
@@ -0,0 +1,5 @@
+package ImplementacionDAO;
+
+public interface ExceptionDAO implements Ex{
+
+}
